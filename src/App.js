09b1a00bb50076397a7c08351d950be2b07efdb0
Nouman-Javaid <mjavaid.bscs16seecs@seecs.edu.pk>
2020-10-08 01:18:09 +0500

    reactComponentsPart2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MainComponents';
+import {Dishes} from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary" >
-        <div className='container'>
-          <NavbarBrand href="Alla" >FIFA</NavbarBrand>
-        </div>
-      </Navbar>
+export default class App extends Component{
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+        dishes:Dishes      
+    }
+  
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary" >
+          <div className='container'>
+            <NavbarBrand href="Alla" >FIFA</NavbarBrand>
+          </div>
+        </Navbar>
 
-      <Menu />      
-    </div>
+        <Menu dishes = {this.state.dishes} />      
+      </div>
   );
+  }
 }
-
-export default App;
